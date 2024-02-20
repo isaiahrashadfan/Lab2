dc62507131a7d2f9890c46aec17cc9029c095c60
Chris Tse <147206326+isaiahrashadfan@users.noreply.github.com>
2024-02-20 01:14:05 -0800

    Completed Lab

--- Lab2ChristopherTse.py
+++ Lab2ChristopherTse.py
@@ -26,3 +26,52 @@ def guessing_game():
     replay = str(input('Do you want to play again? (Y/N): '))
     if (replay == 'Y'):
         guessing_game()
+    else:
+        selection()
+
+
+# Rock-Paper-Scissors
+        
+def rps():
+    rnum = random.randint(1,3)
+    inp = str(input('Do you want to play? (Y/N): '))
+    if (inp == 'Y'):
+        choice = int(input('Enter your choice:\t 1. paper\t2. scissors\t3. rock: '))
+        if (rnum == 1):
+            if (choice == 2):
+                print('You won!')
+            elif (choice == 3):
+                print('You lost!')
+            else:
+                print("It's a tie!")
+        if (rnum == 2):
+            if (choice == 3):
+                print('You won!')
+            elif (choice == 1):
+                print('You lost!')
+            else:
+                print("It's a tie!")
+        if (rnum == 3):
+            if (choice == 1):
+                print('You won!')
+            elif (choice == 2):
+                print('You lost!')
+            else:
+                print("It's a tie!")
+        rps()
+    else:
+        selection()
+
+def selection():
+    game = int(input('Which game do you want to play? \t1. Guessing Game\t2. Rock-Paper-Scissors\t3. Exit: '))
+    if(game == 1):
+        guessing_game()
+    elif(game == 2):
+        rps()
+    else:
+        print('Exiting...')
+
+# Main
+        
+if __name__== "__main__":
+    selection()
